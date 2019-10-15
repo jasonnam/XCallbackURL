2a766b31448657ec9ca986c8191a99c98cd0ace0
Jason Nam <contact@jasonnam.com>
2019-10-15 23:31:01 +0700

    Remove platforms

--- Package.swift
+++ Package.swift
@@ -4,10 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "XCallbackURL",
-    platforms: [
-        .macOS(.v10_10),
-        .iOS(.v8)
-    ],
     products: [
         .library(
             name: "XCallbackURL",
